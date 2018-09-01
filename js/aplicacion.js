3df2658d8930b6e064cb2680ad1e0a58278e851b
Mario Basurto <bacec.soluciones@gmail.com>
2018-08-31 20:34:17 -0500

    Commit inicial
    Carga de la aplicación para PhoneGap

---
+++ js/aplicacion.js
@@ -0,0 +1,38 @@
+function buscarNegocio(codigo)
+{
+    localStorage.setItem("codigo",codigo);
+    window.location = "detalles.html";
+}
+
+function pedirNegocio(codigo)
+{
+    document.getElementById('codigo').value = localStorage.getItem("codigo",codigo);
+    toJSONString('test','res');
+    /*localStorage.removeItem("codigo");*/
+}
+
+function verVacante(codigo)
+{
+    localStorage.setItem("codigoVacante",codigo);
+    window.location = "vacante.html";
+}
+
+function pedirVacante(codigo)
+{
+    document.getElementById('codigo').value = localStorage.getItem("codigoVacante",codigo);
+    toJSONString('test','res');
+    /*localStorage.removeItem("codigoVacante");*/
+}
+
+function verPromocion(codigo)
+{
+    localStorage.setItem("codigoPromocion",codigo);
+    window.location = "promocion.html";
+}
+
+function pedirPromocion(codigo)
+{
+    document.getElementById('codigo').value = localStorage.getItem("codigoPromocion",codigo);
+    toJSONString('test','res');
+    /*localStorage.removeItem("codigoPromocion");*/
+}
